202849f1c3791479543e45d2085b3f346545d1d0
Aryankumar011 <CSE_63@gecpat.gujgov.edu.in>
2023-05-17 10:28:09 +0530

    Menu Bar Add

--- src/Text_Editor.java
+++ src/Text_Editor.java
@@ -1,5 +1,7 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
@@ -17,6 +19,14 @@ public class Text_Editor extends Application{
      // Image image = new Image("image\\Pen.png");
       stage.getIcons().add(image);
       stage.setResizable(false);
+      MenuBar menuBar = new MenuBar();
+      Menu Menu1 = new Menu("Test1");
+      Menu Menu2 = new Menu("Test2");
+      Menu Menu3 = new Menu("Test3");
+      Menu Menu4 = new Menu("Test4");
+      Menu Menu5 = new Menu("Test5");
+      menuBar.getMenus().addAll(Menu1,Menu2,Menu3,Menu4,Menu5);
+      borderPane.setTop(menuBar);
       scene.setFill(Color.AQUA);
       stage.show();
         
